3f2fa92da3681b3fd7c42a329b7276109f94cd37
Irfan Khatik <irfan.khatik@gmail.com>
2021-03-29 07:09:46 +0530

    Added more unit tests.

---
+++ AdiChallengeTests/AdiChallengeTests.swift
@@ -0,0 +1,42 @@
+//
+//  AdiChallengeTests.swift
+//  AdiChallengeTests
+//
+//  Created by Irfan Khatik on 29/03/21.
+//
+
+import XCTest
+import Combine
+
+@testable import AdiChallenge
+
+class AdiChallengeTests: XCTestCase {
+    //store ongoing subscribers (ongoing calls) here, for simplicities sake.
+    var subscribers = Set<AnyCancellable>()
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        //NOTE: Subscribers are cancelled on deinit, so realistically the removeAll call is all that is needed to stop any ongoing calls.
+        subscribers.forEach { $0.cancel() }
+        subscribers.removeAll()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        NetworkService().fetchProducts()
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
